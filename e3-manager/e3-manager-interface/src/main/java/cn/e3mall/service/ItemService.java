115e4da70507a7c8e0bdc5eec8cc6c0660948f21
Tom <Tom@DESKTOP-EI1PHDJ>
2017-06-30 21:15:04 +0800

    第三天

--- e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,6 +1,9 @@
 package cn.e3mall.service;
 
+import java.util.List;
+
 import cn.e3mall.common.pojo.DataGridResult;
+import cn.e3mall.common.pojo.TreeNode;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
@@ -8,4 +11,6 @@ public interface ItemService {
 	TbItem getItemById(long itemId);
 
 	DataGridResult getItemList(int page, int rows);
+
+	
 }
