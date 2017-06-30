3c7c1a5baacd719f89e08f6ee1a09a4649b23994
Tom <Tom@DESKTOP-EI1PHDJ>
2017-06-30 09:45:33 +0800

    第二天

--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,12 +1,25 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
-
+/**
+ * 商品管理Service
+ * <p>Title: ItemServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
 @Service
 public class ItemServiceImpl implements ItemService{
 
@@ -19,4 +32,20 @@ public class ItemServiceImpl implements ItemService{
 		return item;
 	}
 
+	@Override
+	public DataGridResult getItemList(int page, int rows) {
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//执行查询
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		// 取分页结果
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+		//返回结果
+		DataGridResult result = new DataGridResult();
+		result.setRows(list);
+		result.setTotal(total);
+		return result;
+	}
 }
